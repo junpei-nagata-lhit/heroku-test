f7ef639ec5a9e81c2c1c5436cbbcf79dabb1d870
Junpei Nagata <junpei.nagata@lhit.co.jp>
2022-01-28 16:35:30 +0900

    add directory check

--- src/write_test.py
+++ src/write_test.py
@@ -1,2 +1,10 @@
-with open("output/test".txt) as f:
+import os
+
+outdir = "output"
+outfile = os.path.join(outdir, "test.txt")
+
+if os.path.isdir(outdir):
+    os.makedirs(outdir, exist_ok=True)
+
+with open(outfile) as f:
     f.write("test...")
